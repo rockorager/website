d6c3d6cf776c9029e12b17915b554139ef8ef58a
Brandon Romano <BrandonRRomano@gmail.com>
2024-08-19 12:13:20 -0700

    Generate proper jumplink IDs for complex headers
    ```
    ```

--- src/components/jumplink-header/index.tsx
+++ src/components/jumplink-header/index.tsx
@@ -15,9 +15,7 @@ export default function JumplinkHeader({
   children,
   as,
 }: JumplinkHeaderProps) {
-  const id = slugify(children as string, {
-    lower: true,
-  });
+  const id = headerDeeplinkIdentifier(children);
   return (
     <div className={s.jumplinkHeader} id={id}>
       <div
@@ -39,4 +37,45 @@ export default function JumplinkHeader({
       </div>
     </div>
   );
+}
+
+/**
+ * @param children the MDX children node of the header element
+ * @returns The resulting id string which should be applied to the jumplink-header
+ */
+function headerDeeplinkIdentifier(children?: React.ReactNode): string {
+  const unexpectedError = () => {
+    return new Error(`We have encountered an unsupported MDX element in a Header,
+and are unable to generate an identifier for it. This likely a bug
+with the function which threw this error.
+
+Header MDX:
+${JSON.stringify(children, null, 2)}`);
+  };
+
+  if (typeof children === "string") {
+    // # Some simple string header
+    return slugify(children, { lower: true });
+  } else if (Array.isArray(children)) {
+    // # Some `multi-component` header
+    var flattenedTitle = "";
+    children.forEach((value) => {
+      if (typeof value === "string") {
+        flattenedTitle += value;
+      } else if (typeof value === "object" && value.type === "code") {
+        flattenedTitle += value.props.children;
+      } else {
+        throw unexpectedError();
+      }
+    });
+    return slugify(flattenedTitle, { lower: true });
+  } else if (children != null && typeof children === "object") {
+    if ((children as any).type === "code") {
+      // # `just-some-code`
+      return slugify((children as any).props.children, { lower: true });
+    }
+    throw unexpectedError();
+  } else {
+    throw unexpectedError();
+  }
 }
