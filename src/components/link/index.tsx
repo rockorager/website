17b3be4c2731d7506f6b66c85bbb37be87223136
Brandon Romano <BrandonRRomano@gmail.com>
2024-12-01 12:49:41 -0800

    Adds font weight support to links

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -1,15 +1,26 @@
-import s from "./Link.module.css";
+import classNames from "classnames";
 import { SquareArrowOutUpRight } from "lucide-react";
+import { pretendardVariable } from "../text";
+import s from "./Link.module.css";
 
 export interface LinkProps {
   text: string;
   href: string;
+  weight?: "light" | "regular" | "medium";
 }
 
-export default function Link({ text, href }: LinkProps) {
+export default function Link({ text, href, weight = "light" }: LinkProps) {
   const isExternal = !href.startsWith("/");
   return (
-    <a className={s.link} href={href} target={isExternal ? "_blank" : ""}>
+    <a
+      className={classNames(s.link, pretendardVariable.className, {
+        [s.weightLight]: weight === "light",
+        [s.weightRegular]: weight === "regular",
+        [s.weightMedium]: weight === "medium",
+      })}
+      href={href}
+      target={isExternal ? "_blank" : ""}
+    >
       {text} {isExternal && <SquareArrowOutUpRight size={16} />}
     </a>
   );
