9dbcfe29a498c09eec231e89ab47f48a94f8c5ea
Brandon Romano <BrandonRRomano@gmail.com>
2024-12-01 13:26:05 -0800

    Updates footer ot use GridContainer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import GridContainer, { NavAndFooterGridConfig } from "../grid-container";
 import Link, { LinkProps } from "../link";
 import { P } from "../text";
 import s from "./Footer.module.css";
@@ -12,18 +13,23 @@ interface FooterProps {
 export default function Footer({ className, links, copyright }: FooterProps) {
   return (
     <footer className={classNames(s.footer, className)}>
-      {links && (
-        <ul className={s.linkList}>
-          {links.map((link) => {
-            return (
-              <li key={link.text}>
-                <Link {...link} />
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      <P>{copyright}</P>
+      <GridContainer
+        className={s.gridContainer}
+        gridConfig={NavAndFooterGridConfig}
+      >
+        {links && (
+          <ul className={s.linkList}>
+            {links.map((link) => {
+              return (
+                <li key={link.text}>
+                  <Link {...link} />
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        <P>{copyright}</P>
+      </GridContainer>
     </footer>
   );
 }
