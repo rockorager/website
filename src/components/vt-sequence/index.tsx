4ae94af6952b72c57c157d3e5d4cd087dcf154cd
Kyaw <mail@kyswtn.com>
2024-12-21 21:10:18 +0700

    feat: add vtsequence stylings

--- src/components/vt-sequence/index.tsx
+++ src/components/vt-sequence/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import s from "./VTSequence.module.css";
 
 interface VTSequenceProps {
@@ -13,45 +14,24 @@ interface VTSequenceProps {
 //   - Pn will be considered a parameter
 //
 export default function VTSequence(props: VTSequenceProps) {
-  const {
-    sequence,
-    unimplemented = false,
-  } = props;
-  let arr: [string] = typeof sequence === "string" ? [sequence] : sequence;
+  const { sequence, unimplemented = false } = props;
+  const sequenceElements = useMemo(() => parseSequence(sequence), [sequence]);
 
   // TODO: styling if unimplemented is set
 
-  if (arr[0] === "CSI") {
-    arr.shift();
-    arr.unshift("ESC", "[");
-  }
-
   return (
     <div className={s.vtsequence}>
-      {arr.map((elem, i) => (
-        <div key={`${i}${elem}`} className="shrink">
-          <VTElem elem={elem} />
+      {sequenceElements.map(({ value, hex }, i) => (
+        <div key={i} className={s.vtelem}>
+          <div>{hex ? `0x${hex}` : "____"}</div>
+          <div>{value}</div>
         </div>
       ))}
     </div>
   );
 }
 
-function VTElem({ elem }: { elem: string }) {
-  const param = elem.length > 1 && elem[0] === "P";
-  elem = param ? elem[1] : elem;
-  const specialChar = special[elem] ?? elem.charCodeAt(0);
-  const hex = specialChar.toString(16).padStart(2, "0").toUpperCase();
-
-  return (
-    <div className="vtelem">
-      <div>{param ? "____" : `0x${hex}`}</div>
-      <div>{elem}</div>
-    </div>
-  );
-}
-
-const special: { [key: string]: number } = {
+const special: Record<string, number> = {
   BEL: 0x07,
   BS: 0x08,
   TAB: 0x09,
@@ -59,3 +39,22 @@ const special: { [key: string]: number } = {
   CR: 0x0d,
   ESC: 0x1b,
 };
+
+function parseSequence(sequence: string | string[]) {
+  let sequenceArray = typeof sequence === "string" ? [sequence] : sequence;
+  if (sequenceArray[0] === "CSI") {
+    sequenceArray.shift();
+    sequenceArray.unshift("ESC", "[");
+  }
+
+  return sequenceArray.map((value) => {
+    // Pn is a param with name n.
+    const param = value.match(/\P(\w)/)?.[1];
+    if (param) return { value: param };
+
+    // All sequence elements have hex except params.
+    const specialChar = special[value] ?? value.charCodeAt(0);
+    const hex = specialChar.toString(16).padStart(2, "0").toUpperCase();
+    return { value, hex };
+  });
+}
