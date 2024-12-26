8ee31526a404a66e3c394f8eeb87f413b7e78953
Liam Canetti <git@liamcanetti.co.uk>
2024-12-26 19:07:07 +0000

    chore(generic-card): padding as prop

--- src/components/generic-card/index.tsx
+++ src/components/generic-card/index.tsx
@@ -1,19 +1,22 @@
+import { UnitProp } from "@/types/style";
 import { H2, P } from "../text";
 import s from "./GenericCard.module.css";
 
 interface GenericCardProps {
   title: string;
   description: string;
+  padding?: UnitProp;
   children?: React.ReactNode;
 }
 
 export default function GenericCard({
   title,
+  padding = "56px",
   description,
   children,
 }: GenericCardProps) {
   return (
-    <div className={s.genericCard}>
+    <div className={s.genericCard} style={{ padding }}>
       <H2 className={s.title}>{title}</H2>
       <P className={s.description}>{description}</P>
       <div className={s.buttons}>{children}</div>
