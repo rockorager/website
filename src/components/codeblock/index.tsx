579f528a07298fb8cb809847ca58da9ca73c1994
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2024-12-27 01:06:48 +0300

    add copy button to code blocks

--- src/components/codeblock/index.tsx
+++ src/components/codeblock/index.tsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
 import { jetbrainsMono } from "../text";
 import s from "./CodeBlock.module.css";
+import { useCallback, useState } from "react";
+import { CheckIcon, CopyIcon } from "lucide-react";
 
 interface CodeblockProps {
   children?: React.ReactNode;
@@ -10,6 +12,34 @@ export default function CodeBlock({ children }: CodeblockProps) {
   return (
     <pre className={classNames(s.codeBlock, jetbrainsMono.className)}>
       {children}
+      <CopyButton />
     </pre>
+  );
+}
+
+function CopyButton() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyToClipboard = useCallback(
+    async (e: React.MouseEvent<HTMLButtonElement>) => {
+      await navigator.clipboard.writeText(
+        e.currentTarget.parentNode?.textContent ?? "",
+      );
+      setIsCopied(true);
+      const timeout = setTimeout(() => setIsCopied(false), 1000);
+      return () => clearTimeout(timeout);
+    },
+    [],
+  );
+
+  return (
+    <button
+      className={classNames(s.copyButton, isCopied && s.copyButtonSuccess)}
+      type="button"
+      aria-label={isCopied ? "Copied!" : "Copy to clipboard"}
+      onClick={copyToClipboard}
+    >
+      {isCopied ? <CheckIcon size={20} /> : <CopyIcon size={20} />}
+    </button>
   );
 }
