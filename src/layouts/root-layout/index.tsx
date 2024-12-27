4ae21f2b3db9d03ac6ba6cac972e624468021e09
Brandon Romano <BrandonRRomano@gmail.com>
2024-12-26 18:49:39 -0800

    I think twitter wants the full URL

--- src/layouts/root-layout/index.tsx
+++ src/layouts/root-layout/index.tsx
@@ -43,7 +43,10 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="favicon-16.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta property="og:image" content="/social-share-card.jpg" />
-        <meta name="twitter:image" content="/social-share-card.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://ghostty.org/social-share-card.jpg"
+        />
 
         <meta name="darkreader-lock" />
       </Head>
